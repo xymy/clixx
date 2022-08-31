8e0194f0781a98be6674a44cc0c094dfffd91bcb
xymy <thyfan@163.com>
2022-08-31 20:34:28 +0800

    Refactor exceptions & add signals

--- src/clixx/exceptions.py
+++ src/clixx/exceptions.py
@@ -1,8 +1,10 @@
-class ClixxException(Exception):
+class CLIXXException(Exception):
+    """The base class for all CLIXX exceptions."""
+
     exit_code = 128
 
 
-class DefinitionError(ClixxException):
+class DefinitionError(CLIXXException):
     exit_code = 129
 
 
@@ -10,7 +12,7 @@ class GroupDefinitionError(DefinitionError):
     exit_code = 130
 
 
-class UsageError(ClixxException):
+class UsageError(CLIXXException):
     exit_code = 137
 
 
@@ -18,6 +20,15 @@ class GroupUsageError(UsageError):
     exit_code = 138
 
 
-class CLIExit(ClixxException):
-    def __init__(self, exit_code: int = 0) -> None:
-        self.exit_code = exit_code
+class CLIXXSignal(BaseException):
+    """The base class for all CLIXX signals."""
+
+    exit_code = 0
+
+
+class HelpSignal(CLIXXSignal):
+    """The signal for showing help information."""
+
+
+class VersionSignal(CLIXXSignal):
+    """The signal for showing version information."""
