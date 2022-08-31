87e4a9ec8f5ddd5487c5ec89338858d5c2dd19dc
xymy <thyfan@163.com>
2022-08-31 21:00:11 +0800

    Add abbr for GroupType

--- src/clixx/groups.py
+++ src/clixx/groups.py
@@ -4,15 +4,23 @@ from typing import List, Iterator
 from .arguments import Argument, OptionBase
 
 
-class GT(Enum):
+class GroupType(Enum):
     """The group type."""
 
     ANY = auto()
     ALL = auto()
     NONE = auto()
-    AT_MOST_ONE = auto()
     AT_LEAST_ONE = auto()
+    AT_MOST_ONE = auto()
     EXACTLY_ONE = auto()
+
+
+ANY = GroupType.ANY
+ALL = GroupType.ALL
+NONE = GroupType.NONE
+AT_LEAST_ONE = GroupType.AT_LEAST_ONE
+AT_MOST_ONE = GroupType.AT_MOST_ONE
+EXACTLY_ONE = GroupType.EXACTLY_ONE
 
 
 class ArgumentGroup:
@@ -32,7 +40,7 @@ class ArgumentGroup:
 class OptionGroup:
     """The group of optional arguments."""
 
-    def __init__(self, name: str, *, type: GT = GT.ANY) -> None:
+    def __init__(self, name: str, *, type: GroupType = ANY) -> None:
         self.name = name
         self.type = type
         self.gourp: List[OptionBase] = []
