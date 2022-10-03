f30c9a39f9bbdf7285d186c6ff0c4f0788651dcb
xymy <thyfan@163.com>
2022-10-03 11:12:04 +0800

    Use weakref to parent

--- src/clixx/context.py
+++ src/clixx/context.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
+import weakref
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, cast
 
 from .arguments import Argument, Option
 from .exceptions import DefinitionError
@@ -9,9 +10,9 @@ from .groups import ArgumentGroup, OptionGroup
 
 
 class ArgumentNode:
-    def __init__(self, argument: Argument, parent: int) -> None:
+    def __init__(self, argument: Argument, parent: ArgumentGroupNode) -> None:
         self.argument = argument
-        self.parent = parent
+        self.parent = cast(ArgumentGroupNode, weakref.proxy(parent))
         self.occurred = False
 
     def store(self, args: dict[str, Any], value: str) -> None:
@@ -34,9 +35,9 @@ class ArgumentGroupNode:
 
 
 class OptionNode:
-    def __init__(self, option: Option, parent: int) -> None:
+    def __init__(self, option: Option, parent: OptionGroupNode) -> None:
         self.option = option
-        self.parent = parent
+        self.parent = cast(OptionGroupNode, weakref.proxy(parent))
         self.occurred = False
 
     def store(self, args: dict[str, Any], value: str, *, key: str) -> None:
@@ -64,22 +65,24 @@ class OptionGroupNode:
 
 def _build_argument_tree(argument_groups: list[ArgumentGroup]) -> list[ArgumentGroupNode]:
     tree: list[ArgumentGroupNode] = []
-    for index, group in enumerate(argument_groups):
-        children: list[ArgumentNode] = []
+    for group in argument_groups:
+        group_node = ArgumentGroupNode(group, [])
+        tree.append(group_node)
         for argument in group:
-            children.append(ArgumentNode(argument, index))
-        tree.append(ArgumentGroupNode(group, children))
+            node = ArgumentNode(argument, group_node)
+            group_node.children.append(node)
     return tree
 
 
 def _build_option_tree(option_groups: list[OptionGroup]) -> tuple[list[OptionGroupNode], dict[str, OptionNode]]:
     tree: list[OptionGroupNode] = []
     lookup: dict[str, OptionNode] = {}
-    for index, group in enumerate(option_groups):
-        children: list[OptionNode] = []
+    for group in option_groups:
+        group_node = OptionGroupNode(group, [])
+        tree.append(group_node)
         for option in group:
-            node = OptionNode(option, index)
-            children.append(node)
+            node = OptionNode(option, group_node)
+            group_node.children.append(node)
             for key in option.long_options:
                 if key in lookup:
                     raise DefinitionError(f"Option {key!r} conflicts.")
@@ -88,7 +91,6 @@ def _build_option_tree(option_groups: list[OptionGroup]) -> tuple[list[OptionGro
                 if key in lookup:
                     raise DefinitionError(f"Option {key!r} conflicts.")
                 lookup[key] = node
-        tree.append(OptionGroupNode(group, children))
     return tree, lookup
 
 
