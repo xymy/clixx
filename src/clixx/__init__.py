fcdfb81a67574ce0fc7026eeb747c703d02d9b8d
xymy <thyfan@163.com>
2022-10-11 18:10:48 +0800

    Export APIs

--- src/clixx/__init__.py
+++ src/clixx/__init__.py
@@ -1,3 +1,38 @@
+from .arguments import Argument, CountOption, FlagOption, Option, SignalOption
+from .groups import ALL, ANY, AT_LEAST_ONE, AT_MOST_ONE, EXACTLY_ONE, NONE, ArgumentGroup, GroupType, OptionGroup
+from .types import Bool, Choice, DateTime, DirPath, File, FilePath, Float, Int, Path, Str, Type
+
+__all__ = [
+    # arguments
+    "Argument",
+    "Option",
+    "FlagOption",
+    "CountOption",
+    "SignalOption",
+    # groups
+    "GroupType",
+    "ANY",
+    "ALL",
+    "NONE",
+    "AT_LEAST_ONE",
+    "AT_MOST_ONE",
+    "EXACTLY_ONE",
+    "ArgumentGroup",
+    "OptionGroup",
+    # types
+    "Type",
+    "Str",
+    "Bool",
+    "Int",
+    "Float",
+    "Choice",
+    "DateTime",
+    "File",
+    "Path",
+    "DirPath",
+    "FilePath",
+]
+
 __title__ = "clixx"
 __version__ = "0.2.0a"
 __author__ = "xymy"
