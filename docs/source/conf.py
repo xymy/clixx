574693358fa2c854d1d8ceee0400c0f6fde27054
xymy <thyfan@163.com>
2022-10-15 09:57:57 +0800

    Init docs

---
+++ docs/source/conf.py
@@ -0,0 +1,79 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+import os
+import sys
+from importlib import import_module
+from pathlib import Path
+
+src_dir = Path(__file__).resolve().parents[2].joinpath("src")
+sys.path.insert(0, os.fsdecode(src_dir))
+clixx = import_module("clixx")
+
+# -- Project information -----------------------------------------------------
+
+project = "CLIXX"
+copyright = "2022, xymy"
+author = "xymy"
+
+version = clixx.__version__
+release = clixx.__version__
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "myst_parser",
+    "sphinx_copybutton",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = "furo"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+# -- Options for intersphinx -------------------------------------------------
+
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
+
+# -- Options for myst-parser -------------------------------------------------
+
+myst_enable_extensions = [
+    "deflist",
+    "dollarmath",
+    "linkify",
+    "tasklist",
+]
+
+# -- Options for sphinx-copybutton -------------------------------------------
+
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+copybutton_prompt_is_regexp = True
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. CLIXX documentation master file, created by
+   sphinx-quickstart on Sat Oct 15 09:44:45 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to CLIXX's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
