4daa2dc520726ccd1d26ce1b4fcc8ea86659038d
xymy <thyfan@163.com>
2022-10-23 10:24:27 +0800

    Improve command printer

--- src/clixx/commands.py
+++ src/clixx/commands.py
@@ -4,7 +4,7 @@ import sys
 from contextlib import contextmanager
 from typing import Any, Callable, Generator, Protocol
 
-from .exceptions import CLIXXException
+from .exceptions import CLIXXException, HelpSignal, VersionSignal
 from .groups import ArgumentGroup, OptionGroup
 from .parsers import Parser
 
@@ -12,7 +12,7 @@ from .parsers import Parser
 class Printer(Protocol):
     """The protocol class for printer."""
 
-    def print_error(self, cmd: Command, error: CLIXXException) -> None:
+    def print_error(self, cmd: Command, message: str) -> None:
         ...
 
     def print_help(self, cmd: Command) -> None:
@@ -22,23 +22,34 @@ class Printer(Protocol):
         ...
 
 
+#: The type of printer factory.
 PrinterFactory = Callable[[dict[str, Any]], Printer]
+
+
+def _rich_printer_factory(config: dict[str, Any]) -> Printer:
+    from ._rich import RichPrinter
+
+    return RichPrinter(config)
 
 
 class Command:
     def __init__(
-        self, name: str | None = None, version: str | None = None, *, config: dict[str, Any] | None = None
+        self,
+        name: str | None = None,
+        version: str | None = None,
+        *,
+        printer_factory: PrinterFactory | None = None,
+        printer_config: dict[str, Any] | None = None,
     ) -> None:
         self.name = name
         self.version = version
 
-        config = {} if config is None else config
-        config.setdefault("try_help_option", "--help")
-        self.config = config
-
         self.parent = None
         self.argument_groups: list[ArgumentGroup] = []
         self.option_groups: list[OptionGroup] = []
+
+        self.printer_factory = printer_factory
+        self.printer_config = printer_config
 
     def add_argument_group(self, *args, **kwargs) -> ArgumentGroup:
         group = ArgumentGroup(*args, **kwargs)
@@ -63,11 +74,33 @@ class Command:
         try:
             yield None
         except CLIXXException as e:
-            from ._rich import echo
-
-            message = f"Error: {e.format_message()}"
-            echo(message, fg="red", bold=True, file=sys.stderr)
+            self.print_error(e.format_message())
             sys.exit(e.exit_code)
+        except HelpSignal as e:
+            self.print_help()
+            sys.exit(e.exit_code)
+        except VersionSignal as e:
+            self.print_version()
+            sys.exit(e.exit_code)
+
+    def make_printer(self) -> Printer:
+        if (factory := self.printer_factory) is None:
+            factory = _rich_printer_factory
+        if (config := self.printer_config) is None:
+            config = {}
+        return factory(config)
+
+    def print_error(self, message: str) -> None:
+        printer = self.make_printer()
+        printer.print_error(self, message)
+
+    def print_help(self) -> None:
+        printer = self.make_printer()
+        printer.print_help(self)
+
+    def print_version(self) -> None:
+        printer = self.make_printer()
+        printer.print_version(self)
 
     def get_prog(self) -> str:
         return sys.argv[0] if self.name is None else self.name
