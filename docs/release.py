3817c37f81b90f441ce4c8db4d410c80c1803124
xymy <thyfan@163.com>
2023-04-27 17:00:20 +0800

    Add --clean option to docs/release.py

--- docs/release.py
+++ docs/release.py
@@ -1,31 +1,31 @@
+import argparse
 import os
+import shutil
 import subprocess
 import sys
 import tarfile
-from importlib import import_module
 from pathlib import Path
-
-PKG_NAME = "clixx"
-
-if not (python := sys.executable):
-    print("Error: Python is unable to retrieve the real path to its executable.", file=sys.stderr)
-    sys.exit(1)
 
 src_dir = Path(__file__).resolve().parents[1].joinpath("src")
 sys.path.insert(0, os.fsdecode(src_dir))
-pkg = import_module(PKG_NAME)
+import clixx as pkg  # noqa
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--clean", action="store_true", help="clean build directory before building")
+args = parser.parse_args()
 
 docs_dir = Path(__file__).resolve().parent
 build_dir = docs_dir / "build"
-build_dir.mkdir(parents=True, exist_ok=True)
 dist_dir = docs_dir / "dist"
-dist_dir.mkdir(parents=True, exist_ok=True)
-
 doctrees_dir = build_dir / "doctrees"
 source_dir = docs_dir / "source"
 html_dir = build_dir / "html"
-subprocess.run([python, "-m", "sphinx.cmd.build", "-b", "html", "-d", doctrees_dir, source_dir, html_dir])
 
+if args.clean and build_dir.exists():
+    shutil.rmtree(build_dir)
+subprocess.run([sys.executable, "-m", "sphinx.cmd.build", "-b", "html", "-d", doctrees_dir, source_dir, html_dir])
+
+dist_dir.mkdir(parents=True, exist_ok=True)
 name = f"{pkg.__title__}-{pkg.__version__}-doc"
 with tarfile.open(dist_dir / f"{name}.tar.gz", "w:gz") as tar:
     tar.add(html_dir, name)
